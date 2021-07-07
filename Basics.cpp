8bfbf608c7dcc6a084f3d5aead7102a8acf4df94
Keshav Garg <keshavssgarg@gmail.com>
2021-07-08 03:52:33 +0530

    Create Basics.cpp

---
+++ Basics.cpp
@@ -0,0 +1,30 @@
+/*
+
+Common ways to structure code:
+
+-- Procedural Programming: The program moves through a linear series of instructions.
+-- Functional Programming: The program moves from one function to another.
+-- Object Oriented Programming: Use of Objects and Classes
+
+
+What is Object Oriented Programming?
+
+>> OOP is a programming pattern bassed on the concept of objects which can contain data in the form of
+   fields/attributes/properties and code in the form of functions/ methods / procedures.
+   (Every real life object has some properties and functionalities. We can represent it using OOP)
+
+>> OOP can represent the real world more directly and precisely than previous paradigms, so the program becomes simpler and easier to understand.
+>> Object-Oriented-Programing allows programmers to think of software development as if they are working with real-life entities.
+   In your everyday life, people have the knowledge and can-do various works/tasks.
+   
+ 
+ Four main benefits of OOP
+ 1. Inheritance: The capability of a class to derive properties and characteristics from another class is called Inheritance. 
+                 Inheritance is one of the most important feature of Object Oriented Programming.
+                 (helps in avoiding duplication for example if we have to represent 4 different cars)
+*/
+
+class subclass_name : access_mode base_class_name
+{
+  //body of subclass
+};
